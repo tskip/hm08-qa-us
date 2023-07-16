4af04c5a3806307cadc0039aa592ef44ef4a672c
Tyrus Skipper <tskip06@gmail.com>
2023-07-15 22:51:03 -0400

    Needed to remove all unneccary comment blocks

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -3,76 +3,76 @@ const helper = require('../../helper');
 
 describe('Create an order', () => {
 
-    // it ('Test 1 - Should set the address', async () => {
-    //     //Step 1 - Setting the address
-    //     await browser.url(`/`)
-    //     await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+    it ('Test 1 - Should set the address', async () => {
+        //Step 1 - Setting the address
+        await browser.url(`/`)
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
-    // })
+    })
 
-    // it ('Test 2 - Should select the Supportive class', async () => {
-    //     await browser.url(`/`)
-    //     await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+    it ('Test 2 - Should select the Supportive class', async () => {
+        await browser.url(`/`)
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
-    //     //Calls a function to selecte the supportive class
-    //     await page.selectSupportiveClass();
+        //Calls a function to selecte the supportive class
+        await page.selectSupportiveClass();
 
-    // })
+    })
 
-    // it ('Test 3 - Should fill in the phone number', async () => {
-    //     await browser.url(`/`)
-    //     await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+    it ('Test 3 - Should fill in the phone number', async () => {
+        await browser.url(`/`)
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
-    //     //Enter phone number in the phone number field
-    //     const phoneNumber = await (helper.getPhoneNumber("+1"));
-    //     await page.submitPhoneNumber(phoneNumber);
+        //Enter phone number in the phone number field
+        const phoneNumber = await (helper.getPhoneNumber("+1"));
+        await page.submitPhoneNumber(phoneNumber);
 
-    // })
+    })
 
-    // it ('Test 4 - Should add a credit card', async () => {
-    //     await browser.url(`/`)
-    //     await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+    it ('Test 4 - Should add a credit card', async () => {
+        await browser.url(`/`)
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
-    //     //Add a credit card
-    //     await page.addPaymentMethodCard();
+        //Add a credit card
+        await page.addPaymentMethodCard();
 
-    //     //check for icon for payment method to be a credit card
-    //     const cardPaymentMethodIcon = await $(page.cardPaymentMethodIcon);
-    //     await expect(await $(cardPaymentMethodIcon)).toBeExisting();
-    // })
+        //check for icon for payment method to be a credit card
+        const cardPaymentMethodIcon = await $(page.cardPaymentMethodIcon);
+        await expect(await $(cardPaymentMethodIcon)).toBeExisting();
+    })
 
-    // it ('Test 5 - Write a message to the driver', async () => {
-    //     await browser.url(`/`)
-    //     await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+    it ('Test 5 - Write a message to the driver', async () => {
+        await browser.url(`/`)
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
-    //     //Add text to "Message to the driver"
-    //     await page.addMessageToDriver();
-    //     await browser.pause(5000);
-    // })
+        //Add text to "Message to the driver"
+        await page.addMessageToDriver();
+        await browser.pause(5000);
+    })
 
-    // it ('Test 6 - Ordering a Blanket and Handercheifs ', async () => {
-    //     await browser.url(`/`)
-    //     await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+    it ('Test 6 - Ordering a Blanket and Handercheifs ', async () => {
+        await browser.url(`/`)
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
-    //     //Select the Supportive class
-    //     await page.selectSupportiveClass();
+        //Select the Supportive class
+        await page.selectSupportiveClass();
 
-    //     //pause to make sure everything is caught up
-    //     await browser.pause(1000);
+        //pause to make sure everything is caught up
+        await browser.pause(1000);
 
-    //     //Click the Blanket and Hankerchiefs switch
-    //     await page.orderBlanketAndHankerchief();
+        //Click the Blanket and Hankerchiefs switch
+        await page.orderBlanketAndHankerchief();
 
-    // })
+    })
 
-    // it ('Test 7 - Order 2 Ice creams', async () =>{
-    //     await browser.url(`/`)
-    //     await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+    it ('Test 7 - Order 2 Ice creams', async () =>{
+        await browser.url(`/`)
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
-    //     //order 2 ice creams
-    //     await page.orderIceCream();
+        //order 2 ice creams
+        await page.orderIceCream();
 
-    // })
+    })
 
     it ('Test 8 - Car Search Modal Appears', async () => {
         await browser.url(`/`)
